116f6274fb2ee512744a7ee776041fcea87f2f48
Trung1812 <phamtrungnhom@gmail.com>
2023-07-13 15:16:09 +0700

    Edit controller

---
+++ src/screen/InputArrayController.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button createBtn;
+
+    @FXML
+    private TextArea inputArrayTA;
+
+    @FXML
+    void handleCreateBtn(ActionEvent event) {
+
+    }
+
+}
